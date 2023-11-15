cddaf7ec3f1da477c1cfc77724b7bf763c4fb1d8
EduardoL115 <00367123@uca.edu.sv>
2023-11-14 23:48:10 -0600

    funcion de descansar para el jugador actualizada

--- commit 1/timerVida.cpp
+++ commit 1/timerVida.cpp
@@ -5,36 +5,31 @@
 
 using namespace std;
 
-void displayClock(int seconds, int vida) {
-
-    cout << setfill('0') << setw(2) << seconds << " sec |"<< vida << " vida "<<endl;
+void displayClock(int vida, int seconds) {
+    cout << setfill('0') << setw(2) << seconds << " sec | "<< vida<< " vida "<<endl;
 }
 
-int timer(int vida, int seconds) {
+int Descansar(int vida, int seconds) {
 
     bool timer = true;
-    while (timer && vida <=75) {
-        displayClock(vida, seconds);
+    while (seconds != 30 && vida <75) {
+        displayClock(vida,seconds);
         sleep(1);
-        seconds++;
         vida++;
-
-        if (seconds == 60 || vida==75) {
-            cout<<"Has descansado lo suficiente"<<endl;
-            timer = false;
-            seconds = 0;
-        }
+        seconds++;
     } 
+        cout<<"Has descansado lo suficiente"<<endl;
+        timer = false;
+        seconds = 0;
     return vida;
-}
 
+}
 int main(){
 
-int vida = 0;
-int seconds = 0;
+int vida = 60;
+int seconds = 1;
 
-    int hp = timer(vida,seconds);
-
+    int hp = Descansar(vida,seconds);
     cout<<"Su vida es: "<<hp;
     return 0;
 }
